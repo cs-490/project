3cd59528b152d5ed58282dde664c4856bf3dd6b7
devendra-hiralall <63375910+devendra-hiralall@users.noreply.github.com>
2020-09-29 12:36:47 -0400

    Add files via upload

---
+++ profile.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- FontAwesome CSS-->
+    <script src="https://kit.fontawesome.com/a72b2bc306.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="css/styles.css">
+
+    <title>CS490 Project</title>
+</head>
+
+
+<body>
+    <h1>CS 490 Project</h1>
+    <div class=profileMain>
+        <div class="searchBar">
+            <form action="">
+                <input type="text" placeholder="Search" name="search">
+                <button type="submit"><i class="fa fa-search"></i></button>
+            </form>
+        </div>
+    </div>
+
+
+    <!-- JavaScript -->
+     <script src="js/main.js"></script>
+</body>
+
+
+</html>
