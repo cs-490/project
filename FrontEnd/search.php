dee42ec594e055ead2fa9396a6d5819ece1b5d11
devendra-hiralall <63375910+devendra-hiralall@users.noreply.github.com>
2020-10-06 12:55:54 -0400

    Add files via upload

---
+++ FrontEnd/search.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- FontAwesome CSS-->
+    <script src="https://kit.fontawesome.com/a72b2bc306.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="css/styles.css">
+
+    <title>CS490 Project</title>
+</head>
+
+
+<body>
+
+    <h1 id="title">CS 490 Project</h1>
+
+    <div class="searchMain">
+
+        <div class="searchBar">
+            <form action="">
+                <input type="text" placeholder="Search" name="search">
+                <button type="submit"><i class="fa fa-search"></i></button>
+            </form>
+        </div>
+
+        <a href="profile.php" id="searchLink">Back to profile</a>
+
+        <div class="profileSearch">
+            <h4>Name:</h4>
+            <h4>Announcement:</h4>
+        </div>
+        <div class="profileSearch">
+            <h4>Name:</h4>
+            <h4>Announcement:</h4>
+        </div>
+        <div class="profileSearch">
+            <h4>Name:</h4>
+            <h4>Announcement:</h4>
+        </div>
+    </div>
+
+    <!-- JavaScript -->
+    <script src="js/main.js"></script>
+</body>
+
+
+</html>
